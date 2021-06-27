67e8f736387166d567464f8492ec99c61f6f5eda
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-06-27 16:13:33 +0200

    Style the menu items

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -49,12 +49,13 @@ class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, title, imageUrl }) => {
+        {this.state.sections.map(({ id, title, imageUrl, size }) => {
           return (
             <MenuItem
               key={id}
               title={title.toUpperCase()}
               imageUrl={imageUrl}
+              size={size}
             />
           );
         })}
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 import './MenuItem.scss';
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className='menu-item' style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={size ? `menu-item ${size}` : 'menu-item'}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
