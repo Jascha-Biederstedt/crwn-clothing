c0e6181f4544999b5a92c77e6d5312fd99223fcb
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-06-28 17:26:40 +0200

    Add collection preview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './components/HomePage/HomePage';
+import Shop from './components/Shop/Shop';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={Shop} />
+      </Switch>
     </div>
   );
 }
