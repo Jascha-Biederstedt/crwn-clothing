b480573d79ecda9f9a2542e23b254e6831b733c1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-07-01 18:32:23 +0200

    Add sign up component

--- src/components/SignInAndSignUp/SignInAndSignUp.js
+++ src/components/SignInAndSignUp/SignInAndSignUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from '../SignIn/SignIn';
+import SignUp from '../SignUp/SignUp';
 
 import './SignInAndSignUp.scss';
 
@@ -8,6 +9,7 @@ const SignInAndSignUp = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
