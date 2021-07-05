2f8541ae03e5e0fcf2d86e6f19e0276bd80638df
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-07-05 15:26:08 +0200

    Implement store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './redux/store';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
