3d17ae4685c00cbb5c523618ee53bc2ba3fde305
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-07-12 12:11:23 +0200

    Add and style checkout item

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../CheckoutItem/CheckoutItem';
+
 import { selectCartItems } from '../../redux/cart/cart-selectors';
 import { selectCartTotal } from '../../redux/cart/cart-selectors';
 
@@ -27,7 +29,9 @@ const Checkout = ({ cartItems, total }) => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map(cartItem => cartItem.name)}
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
       <div className='total'>TOTAL: ${total}</div>
     </div>
   );
