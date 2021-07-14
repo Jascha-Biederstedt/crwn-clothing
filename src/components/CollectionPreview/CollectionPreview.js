51951dd44a7fc92871386804c625624360ae0c13
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-07-14 14:45:49 +0200

    Implement routing on shop page

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import CollectionItem from '../CollectionItem/CollectionItem';
 
@@ -7,7 +8,9 @@ import './CollectionPreview.scss';
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <Link className='title' to={`/shop/${title.toLowerCase()}`}>
+        <h2>{title.toUpperCase()}</h2>
+      </Link>
       <div className='preview'>
         {items
           .filter((item, index) => index < 4)
